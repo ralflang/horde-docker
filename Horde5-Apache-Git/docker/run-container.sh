52ee042f930a1f32df8020d7ec6cf5d5481b0713
Ralf Lang <lang@b1-systems.de>
2017-01-06 11:28:40 +0100

    Add db migration TODO: Provide sane default config including db access TODO: Parameters! Don't use this version for anything but local testing

--- Horde5-Apache-Git/docker/run-container.sh
+++ Horde5-Apache-Git/docker/run-container.sh
@@ -4,8 +4,16 @@
 /usr/sbin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib64/mysql/plugin --user=mysql --log-error=/var/log/mysql/mysqld.log --pid-file=/var/run/mysql/mysql.pid --socket=/var/run/mysql/mysql.sock &
 # This is hacky, find a better wait condition
 sleep 10
+## Either use parameters from docker run or externalize base DB setup
 mysqladmin create horde
+mysql -e "GRANT ALL ON horde.* to horde@localhost identified by 'horde';"
+## lock root
 mysqladmin password horde
+
+## For some reason, horde-db-migrate needs more than one run when starting from blank. It's supposed to be idempotent so running it three times won't hurt
+/srv/git/horde/horde/bin/horde-db-migrate
+/srv/git/horde/horde/bin/horde-db-migrate
+/srv/git/horde/horde/bin/horde-db-migrate
 
 # Need to cleanup pid on rerun, otherwise it won't run well
 rm /var/run/apache*.pid
